690a49487b6a1b1ca612e64c1885391dc92afc4e
badcarolwork <badcarol.work@gmail.com>
2025-02-27 14:07:06 +0800

    update header

--- scripts/script.js
+++ scripts/script.js
@@ -1,25 +1,25 @@
-const animation = (el) => {
-    const about = [
-        "Front End Developer",
-        "Web UI/UX Specialist",
-        "Reactjs user",
-        "Cat person",
-        "Food hunter",
-        "Carol Chaw",
-        "Freelancer",
-    ]
+// const animation = (el) => {
+//     const about = [
+//         "Front End Developer",
+//         "Web UI/UX Specialist",
+//         "Reactjs user",
+//         "Cat person",
+//         "Food hunter",
+//         "Carol Chaw",
+//         "Freelancer",
+//     ]
 
-    let current = 0;
+//     let current = 0;
 
 
-    setInterval(() => {
-        el.classList.add('bounce-in-top');
-        el.innerHTML = about[current === about.length ? current = 0 : current++];
-        setTimeout(() => {
-            el.classList.remove('bounce-in-top');
-        }, 1300);
-    }, 2000);
-};
+//     setInterval(() => {
+//         el.classList.add('bounce-in-top');
+//         el.innerHTML = about[current === about.length ? current = 0 : current++];
+//         setTimeout(() => {
+//             el.classList.remove('bounce-in-top');
+//         }, 1300);
+//     }, 2000);
+// };
 
 const handleNavClick = () => {
     const buttons = document.querySelectorAll(".nav-item");
@@ -32,6 +32,20 @@ const handleNavClick = () => {
     });
 }
 
-const el = document.querySelector('.infos .title');
-animation(el);
+// const el = document.querySelector('.infos .title');
+// animation(el);
 handleNavClick();
+
+//// Init Type.sj
+const selectTyped = document.querySelector('.typed');
+if (selectTyped) {
+    let typed_strings = selectTyped.getAttribute('data-typed-items');
+    typed_strings = typed_strings.split(',');
+    new Typed('.typed', {
+        strings: typed_strings,
+        loop: true,
+        typeSpeed: 100,
+        backSpeed: 50,
+        backDelay: 2000
+    });
+}
